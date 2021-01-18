782e612bd9622d18c3e5ea713778b32da841b6b5
Julia <>
2021-01-18 22:30:29 +0100

    first commit

---
+++ data2.js
@@ -0,0 +1,242 @@
+var data2 = [
+  {
+    "yearmonth": "2017-1",
+    "sentinel1": 0,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2017-2",
+    "sentinel1": 8,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2017-3",
+    "sentinel1": 4,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2017-4",
+    "sentinel1": 17,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2017-5",
+    "sentinel1": 281,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2017-6",
+    "sentinel1": 623,
+    "viirs": 9
+  },
+  {
+    "yearmonth": "2017-7",
+    "sentinel1": 703,
+    "viirs": 88
+  },
+  {
+    "yearmonth": "2017-8",
+    "sentinel1": 350,
+    "viirs": 73
+  },
+  {
+    "yearmonth": "2017-9",
+    "sentinel1": 350,
+    "viirs": 138
+  },
+  {
+    "yearmonth": "2017-10",
+    "sentinel1": 387,
+    "viirs": 179
+  },
+  {
+    "yearmonth": "2017-11",
+    "sentinel1": 293,
+    "viirs": 85
+  },
+  {
+    "yearmonth": "2017-12",
+    "sentinel1": 10,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2018-1",
+    "sentinel1": 2,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2018-2",
+    "sentinel1": 4,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2018-3",
+    "sentinel1": 4,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2018-4",
+    "sentinel1": 3,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2018-5",
+    "sentinel1": 531,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2018-6",
+    "sentinel1": 487,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2018-7",
+    "sentinel1": 37,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2018-8",
+    "sentinel1": 10,
+    "viirs": 4
+  },
+  {
+    "yearmonth": "2018-9",
+    "sentinel1": 40,
+    "viirs": 104
+  },
+  {
+    "yearmonth": "2018-10",
+    "sentinel1": 167,
+    "viirs": 246
+  },
+  {
+    "yearmonth": "2018-11",
+    "sentinel1": 122,
+    "viirs": 72
+  },
+  {
+    "yearmonth": "2018-12",
+    "sentinel1": 123,
+    "viirs": 4
+  },
+  {
+    "yearmonth": "2019-1",
+    "sentinel1": 2,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2019-2",
+    "sentinel1": 0,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2019-3",
+    "sentinel1": 0,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2019-4",
+    "sentinel1": 4,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2019-5",
+    "sentinel1": 279,
+    "viirs": 1
+  },
+  {
+    "yearmonth": "2019-6",
+    "sentinel1": 606,
+    "viirs": 5
+  },
+  {
+    "yearmonth": "2019-7",
+    "sentinel1": 162,
+    "viirs": 5
+  },
+  {
+    "yearmonth": "2019-8",
+    "sentinel1": 415,
+    "viirs": 117
+  },
+  {
+    "yearmonth": "2019-9",
+    "sentinel1": 5,
+    "viirs": 193
+  },
+  {
+    "yearmonth": "2019-10",
+    "sentinel1": 285,
+    "viirs": 166
+  },
+  {
+    "yearmonth": "2019-11",
+    "sentinel1": 285,
+    "viirs": 26
+  },
+  {
+    "yearmonth": "2019-12",
+    "sentinel1": 70,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2020-1",
+    "sentinel1": 24,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2020-2",
+    "sentinel1": 0,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2020-3",
+    "sentinel1": 0,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2020-4",
+    "sentinel1": 0,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2020-5",
+    "sentinel1": 95,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2020-6",
+    "sentinel1": 7,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2020-7",
+    "sentinel1": 426,
+    "viirs": 0
+  },
+  {
+    "yearmonth": "2020-8",
+    "sentinel1": 145,
+    "viirs": 1
+  },
+  {
+    "yearmonth": "2020-9",
+    "sentinel1": 66,
+    "viirs": 38
+  },
+  {
+    "yearmonth": "2020-10",
+    "sentinel1": 16,
+    "viirs": 121
+  },
+  {
+    "yearmonth": "2020-11",
+    "sentinel1": 20,
+    "viirs": 86
+  },
+  {
+    "yearmonth": "2020-12",
+    "sentinel1": 28,
+    "viirs": 13
+  }
+]
